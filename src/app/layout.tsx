66ebf0f0f47fac09dd4b4e3f05a6d18ad893fb00
Huỳnh Khang Vỹ <52200024@student.tdtu.edu.vn>
2025-05-29 09:46:55 +0700

    feat : add SEO property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,37 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  description: "A simple and elegant landing page template for SaaS products",
+  openGraph: {
+    title: "Light Saas Landing Page",
+    description: "A simple and elegant landing page template for SaaS products",
+    url: "https://light-sass-landing-two.vercel.app",
+    siteName: "Light Saas Landing Page",
+    type: "website",
+    locale: "en_US",
+
+    images: [
+      {
+        url: "https://res.cloudinary.com/dl8h3byxa/image/upload/v1748486659/Screenshot_3_gyzdud.png",
+        width: 1200,
+        height: 630,
+        alt: "Light Saas Landing Page",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Light Saas Landing Page",
+    description: "A simple and elegant landing page template for SaaS products",
+
+    images: [
+      "https://res.cloudinary.com/dl8h3byxa/image/upload/v1748486659/Screenshot_3_gyzdud.png",
+    ],
+  },
 };
 
 export default function RootLayout({
